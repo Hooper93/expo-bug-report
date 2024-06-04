e1301eb7b4e67ab6bdbb1dea33c994f583947a92
Hooper <admin@hooperui.com>
2024-06-04 19:24:31 +0800

    push

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  function chooseImg() {
+    ImagePicker.launchImageLibraryAsync({
+      allowsEditing: false,
+      quality: 1,
+      exif: true,
+    }).then(result => {
+      if (!result.canceled) {
+        console.log(result.assets[0].exif);
+      } else {
+        alert('You did not select any image.');
+      }
+    });
+  }
+  
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Pressable onPress={chooseImg}>
+        <Text>Choose Image to see EXIF</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
